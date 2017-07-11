b7ca5eef6f6ce43eb1083ec12ffcf756edc76b4f
Eitaro Fukamachi <e.arrows@gmail.com>
2017-07-11 16:52:50 +0900

    Lock in around method of text-logger.

--- logger/text.lisp
+++ logger/text.lisp
@@ -4,13 +4,19 @@
   (:import-from #:local-time)
   (:import-from #:bordeaux-threads)
   (:import-from #:jonathan)
-  (:export #:text-logger))
+  (:export #:text-logger
+           #:text-logger-stream))
 (in-package #:cl-fluent-logger/logger/text)
 
 (defclass text-logger (base-logger)
   ((stream :initarg :stream
-           :initform *standard-output*)
+           :initform *standard-output*
+           :reader text-logger-stream)
    (lock :initform (bt:make-lock))))
+
+(defmethod post-with-time :around ((logger text-logger) tag data time)
+  (bt:with-lock-held ((slot-value logger 'lock))
+    (call-next-method)))
 
 (defmethod post-with-time ((logger text-logger) tag data time)
   (let ((time
@@ -19,21 +25,20 @@
             (local-time:timestamp time)))
         (stream (slot-value logger 'stream))
         (*print-case* :downcase))
-    (bt:with-lock-held ((slot-value logger 'lock))
-      (format stream "~&~A ~A:" time tag)
-      (typecase data
-        (cons
-         (loop for (k . v) in data
-               do (format stream " ~A=~A"
+    (format stream "~&~A ~A:" time tag)
+    (typecase data
+      (cons
+       (loop for (k . v) in data
+             do (format stream " ~A=~A"
+                        k
+                        (uiop:symbol-call :jojo :to-json v))))
+      (hash-table
+       (maphash (lambda (k v)
+                  (format stream " ~A=~A"
                           k
-                          (uiop:symbol-call :jojo :to-json v))))
-        (hash-table
-         (maphash (lambda (k v)
-                    (format stream " ~A=~A"
-                            k
-                            (uiop:symbol-call :jojo :to-json v)))
-                  data))
-        (otherwise
-         (prin1 data stream)))
-      (fresh-line)
-      t)))
+                          (uiop:symbol-call :jojo :to-json v)))
+                data))
+      (otherwise
+       (prin1 data stream)))
+    (fresh-line)
+    t))
